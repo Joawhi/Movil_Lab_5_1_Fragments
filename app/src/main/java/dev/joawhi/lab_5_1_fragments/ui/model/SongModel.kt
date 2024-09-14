5d6a40176f1aae2b4d6613c88a5d7d76e26169b7
Joawhi <joaq11webmovil@gmail.com>
2024-09-14 12:36:42 -0500

    RecyclerView

---
+++ app/src/main/java/dev/joawhi/lab_5_1_fragments/ui/model/SongModel.kt
@@ -0,0 +1,10 @@
+package dev.joawhi.lab_5_1_fragments.ui.model
+
+data class SongModel(
+    val id: Int,
+    val name: String,
+    val album: String,
+    val countPlay: String,
+    val songDuration: String,
+    val artist: String
+)
